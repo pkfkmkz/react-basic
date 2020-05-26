d9f77f19b7851da6575cce5b3908380fec3be195
francisco <francisco@rsolver.com>
2020-05-26 18:30:25 -0400

    ok cors headers

--- todo_api/todo_api/settings.py
+++ todo_api/todo_api/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # todo_api
-    'todos',
     'rest_framework',
+    'corsheaders',
+    'todos',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', # todo_api
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+    "httP://localhost:3000",
+)
