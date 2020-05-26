3d74195f38fffdf37b73a8e5b4489cfd3b82bd58
francisco <francisco@rsolver.com>
2020-05-26 19:09:31 -0400

    ok react list view

---
+++ todo_api/frontend/src/App.js
@@ -0,0 +1,70 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    todos: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/');
+      const todos = await res.json();
+      this.setState({
+        todos
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.todos.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <span>{item.description}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// import React from 'react';
+// import { Button } from '@material-ui/core';
+
+// function App() {
+//   return <Button color="primary">Hello World</Button>;
+// }
+
+// export default App;
